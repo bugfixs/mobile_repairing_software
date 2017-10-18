7d83c4908a103851d625c641a2fc2425a9f4fc3b
DhanshreeK <dhanshree.j.kamble@gmail.com>
2017-10-18 17:11:05 +0530

    set general setting update with current user

--- app/models/general_setting.rb
+++ app/models/general_setting.rb
@@ -1,3 +1,13 @@
 class GeneralSetting < ApplicationRecord
 	has_one :user
+	scope :shod, ->(id) { where(id: id).take }
+	validates :name, presence: true, format: { with: /\A[a-zA-Z.&, " "]+\z/ },
+                                     length: { in: 1..100 }, on: :update
+  	validates :address, presence: true,
+                                        length: { in: 1..100 }, on: :update
+  	validates :contact, presence: true, length: { in: 6..11 },
+                                         format: { with: /\A[0-9]+\z/ }, on: :update
+    validates :email, presence: true, format: \
+  { with: /\A[a-zA-Z0-9._-]+@([a-zA-Z0-9]+\.)+[a-zA-Z]{2,4}+\z/ }
+
 end
