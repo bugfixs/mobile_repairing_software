f96063a222955d7c5bf8afe140905c4f9a257030
DhanshreeK <dhanshree.j.kamble@gmail.com>
2017-10-20 12:53:07 +0530

    removed sign_up, Added user_employee, create user through seed

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,37 @@
 class Employee < ApplicationRecord
   belongs_to :employee_type
+  belongs_to :user, optional:true
+  after_save :create_user_account
+  validates :employee_number, presence: true
+  validates :contact, presence: true
+  validates :email, presence: true, format: \
+  { with: /\A[a-zA-Z0-9._-]+@([a-zA-Z0-9]+\.)+[a-zA-Z]{2,4}+\z/ }
+
+   # this method is used to calculate the employee number
+  # if first employee then append 1 to today date else
+  # append last id to today date
+  def emp_no
+    date = Date.today.strftime('%Y%m%d')
+    self.employee_number = date.to_s + '1' if Employee.first.nil?
+    self.employee_number = date.to_s + \
+                           Employee.last.id.next.to_s unless Employee.first.nil?
+  end
+
+  private
+
+  # This action create the user for employee login.
+  def create_user_account
+    user = User.new do |u|
+      u.email = email
+      u.employee_id = id 
+      u.password = employee_number
+      u.role = 'Employee'
+      u.general_setting_id = if User.current
+                               User.current.general_setting_id
+                             else
+                               1
+                             end
+    end
+    user.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :employees
   resources :employee_types
   resources :general_settings
@@ -7,11 +9,5 @@ Rails.application.routes.draw do
   		get :dashboard
   		end
   	end
-  get 'home/index'
-  
-	devise_scope :user do
-      root to: "devise/sessions#new"
-    end
-	  devise_for :users, controllers: { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
